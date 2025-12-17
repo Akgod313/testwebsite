2e8d38389d943186f3cbe9d15527f39f3a569950
Ayaan Karkera <ayaankarkera8@gmail.com>
2025-12-17 19:20:30 +0530

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,17 @@
 import "~/styles/globals.css";
+import "./page1.css";
 
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
+
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 import { TRPCReactProvider } from "~/trpc/react";
 
@@ -20,10 +30,28 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
+    <ClerkProvider>
+      <html lang="en" className={`${geist.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+        {children}
       </body>
     </html>
+    </ClerkProvider>
+    
   );
 }
+
+
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,12 @@
-import { Client } from "@planetscale/database";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+export const runtime = "nodejs";
 
-import { env } from "~/env";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import * as schema from "./schema";
+import { env } from "~/env";
 
-export const db = drizzle(new Client({ url: env.DATABASE_URL }), { schema });
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
